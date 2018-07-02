c6d163b1e1f91fa9123a3f155a7117c4494e6b06
迷途的指针 <yuyyi51@126.com>
2018-07-02 11:50:04 +0800

    细节优化

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.redirect('/page/signin.html');
+  /*
   let title = 'hello';
   //console.log("index");
   //console.log(req.session);
   if (req.session.user)
     title = req.session.user.username;
   res.render('index', { title: title });
+  */
 });
 /*test1*/
 module.exports = router;
